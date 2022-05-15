da1bce20e79b7dbe47cb0fbfcbbd5d5bfce17216
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2022-05-15 17:56:23 +0700

    added burger menu in header for tablet and mobile device

---
+++ dist/mobile.js
@@ -0,0 +1,22 @@
+let mobileButton = document.getElementById('mobile-btn')
+let win_mobile = document.querySelector('.mobile_navs')
+let header = document.querySelector('.header__wraper')
+
+mobileButton.addEventListener('click', () => {
+    if(mobileButton.className == 'fas fa-bars'){
+        mobileButton.classList.remove('fa-bars')
+        mobileButton.classList.add('fa-times')
+        win_mobile.style.visibility = 'visible'
+        header.style.backgroundColor = '#384D70'
+        document.body.style.overflow = 'hidden'
+    }
+    else{
+        mobileButton.classList.remove('fa-times')
+        mobileButton.classList.add('fa-bars')
+        win_mobile.style.visibility = 'hidden'
+        header.style.backgroundColor = ''
+        document.body.style.overflow = ''
+    }
+
+
+})
